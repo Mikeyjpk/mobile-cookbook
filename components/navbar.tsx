dc172e26bba01cc752730f91ce75a2c37794aebe
Mikeyjpk <michaeljpkerr@gmail.com>
2023-08-29 12:35:06 +1000

    add search input

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,6 +7,7 @@ import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "@/components/mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const font = Montserrat({
     weight: "800",
@@ -15,9 +16,9 @@ const font = Montserrat({
 
 export const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center h-16 py-2 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <Menu className="block md:hidden"/>
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bodl text-primary",
                         font.className
